fe700cc4bdc6621e9ea7f56a78bc58bc247502ed
Den4iK <gerasymenkoden@gmail.com>
2021-11-15 13:33:36 +0100

    add shopping cart service

--- app/components/product.js
+++ app/components/product.js
@@ -1,7 +1,14 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
 
 export default class ProductComponent extends Component {
+  @service shoppingCart;
+
   get image() {
     return `assets/images/${this.args.product.image}`;
   }
+
+  addToCart = () => {
+    this.shoppingCart.add(this.args.product);
+  };
 }
